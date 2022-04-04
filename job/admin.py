181bf5e221c3fcbc94cfe2efe4cd50f96e010d47
Mohamed Bashir <mohamed99bashir@outlook.com>
2022-04-04 10:51:11 +0200

    add job models

--- job/admin.py
+++ job/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+
+'''
+وهنا بضيف الابلكيشن بتاع يوزر في الادمن 
+وبعرف اعدل في شكله في الادمن
+
+'''
+
+from .models import Job
+
+admin.site.register(Job)
--- job/models.py
+++ job/models.py
@@ -1,3 +1,47 @@
+from pydoc import describe
+from pyexpat import model
+from turtle import title
 from django.db import models
 
 # Create your models here.
+
+'''
+Django model field :
+
+    - html Widget
+    - Validation
+    - db size
+
+كل مره بتعدل علي ملف الموديل لازم تكتب امرين :
+
+python manag.py makemigrations
+ودي الديجانو بتشوف اذا ينفع الفيلد يتحول لكويري في الداتا بيز فبيتاكد اذا ينفع ولا لا يتعمل في الداتا بيز , لو مطلعش ايرور ده معناه انه ينفع يتنفذ علي الداتا بيز , علشان انفذه اكتب 
+
+python manage.py migrate 
+
+'''
+
+JOB_TYPE = (
+    ('Full Type', 'Full Type'),
+    ('Part Type', 'Part Type')
+)
+
+
+class Job(models.Model):  # Table
+    title = models.CharField(max_length=100)  # Column
+    # Location
+
+    job_type = models.CharField(max_length=15, choices=JOB_TYPE)
+
+    description = models.TextField(max_length=1000)
+
+    published_at = models.DateTimeField(auto_now=True)
+
+    vacancy = models.IntegerField(default=1)
+
+    salary = models.IntegerField(default=0)
+
+    experience = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.title
