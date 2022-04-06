f7276560ac6c34672ae6f3c6bfb5a6daed39f5ea
Mohamed Bashir <mohamed99bashir@outlook.com>
2022-04-06 10:10:30 +0200

    add front-end templates

--- job/views.py
+++ job/views.py
@@ -1,9 +1,21 @@
+from ast import Return
+from multiprocessing import context
+from pickletools import read_unicodestring1
+import re
 from django.shortcuts import render
-from . import views
+from .models import Job
 # Create your views here.
 
-def job_list(request):
-    pass
 
-def job_detail(request,id):
-    pass
+def job_list(request):
+    # model Query set
+    # https://docs.djangoproject.com/en/4.0/ref/models/querysets/
+    job_list = Job.objects.all()
+    context = {'jobs': job_list}
+    return render(request, 'job/job_list.html', context)
+
+
+def job_detail(request, id):
+    job_detail = Job.objects.get(id=id)
+    context = {'job': job_detail}
+    return render(request, 'job/job_detail.html', context)
